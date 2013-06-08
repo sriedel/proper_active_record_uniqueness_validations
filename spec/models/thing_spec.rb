7ccbef756956d76bb3df7b34ce5eb3063e176d97
Sven Riedel <sr@gimp.org>
2013-06-08 18:48:30 +0200

    Deal with multi-attribute violations

--- spec/models/thing_spec.rb
+++ spec/models/thing_spec.rb
@@ -7,6 +7,12 @@ describe Thing do
                :description => "bar" )
   end
 
+  let( :another_valid_unsaved_thing ) do
+    Thing.new( :some_id     => 10,
+               :name        => "foo_2000",
+               :description => "binford_9000" )
+  end
+
   let( :a_thing_violating_a_single_column_constraint ) do
     Thing.new( :some_id     => 1,
                :name        => "baz",
@@ -19,7 +25,21 @@ describe Thing do
                :description => "bar" )
   end
 
-  describe "#save" do
+  let( :a_widget_with_a_uniqueness_violation ) do
+    Widget.create( :some_id => 1, :name => "foo", :description => "bar" )
+    w = Widget.new( :some_id => 1, :name => "baz", :description => "quux"  )
+    w.save
+    w
+  end
+
+  let( :a_widget_with_a_multi_column_uniqueness_violation ) do
+    Widget.create( :some_id => 1, :name => "foo", :description => "bar" )
+    w = Widget.new( :some_id => 2, :name => "foo", :description => "bar"  )
+    w.save
+    w
+  end
+
+  describe "creating with #save" do
     context "without violating a uniqueness constraint" do
       subject { a_valid_unsaved_thing }
 
@@ -60,7 +80,10 @@ describe Thing do
         expect { subject.save }.to_not change { Thing.count }
       end
 
-      it "should have added an errors entry for the attribute" 
+      it "should have added an errors entry for the attribute" do
+        subject.save
+        subject.errors.messages.should eq( a_widget_with_a_uniqueness_violation.errors.messages )
+      end
     end
 
     context "when violating a multi-column uniqueness constraint" do
@@ -82,7 +105,106 @@ describe Thing do
         expect { subject.save }.to_not change { Thing.count }
       end
 
-      it "should have added an errors entry for the scoped attributes"
+      it "should have added an errors entry for the scoped attributes" do
+        subject.save
+        subject.errors.messages.should eq( a_widget_with_a_multi_column_uniqueness_violation.errors.messages )
+      end
     end
   end
+
+  describe "updating with #save" do
+    context "without violating a uniqueness constraint" do
+      subject { a_valid_unsaved_thing }
+
+      before( :each ) do
+        subject.save 
+        subject.some_id = 3
+      end
+
+      it "should not raise an exception" do
+        expect { subject.save }.to_not raise_error
+      end
+
+      it "should return true" do
+        subject.save.should be_true
+      end
+
+      it "should have stored the value in the database" do
+        expect do
+          subject.save 
+          subject.reload
+        end.to change { subject.updated_at }
+      end
+
+      it "should not have inserted anything to the errors" do
+        subject.save
+        subject.errors.should be_empty
+      end
+    end
+
+    context "when violating a single column uniqueness constraint" do
+      subject { another_valid_unsaved_thing }
+
+      before( :each ) do
+        a_valid_unsaved_thing.save
+        subject.save
+        subject.reload # grab new updated_at
+        subject.some_id = a_valid_unsaved_thing.some_id
+      end
+
+      it "should not raise an exception" do
+        expect { subject.save }.to_not raise_error
+      end
+
+      it "should return false" do
+        subject.save.should be_false
+      end
+
+      it "should not have stored the value in the database" do
+        expect do
+          subject.save
+          subject.reload
+        end.to_not change { subject.updated_at }
+      end
+
+      it "should have added an errors entry for the attribute" do
+        subject.save
+        subject.errors.messages.should eq( a_widget_with_a_uniqueness_violation.errors.messages )
+      end
+    end
+
+    context "when violating a multi-column uniqueness constraint" do
+      subject { another_valid_unsaved_thing }
+
+      before( :each ) do
+        a_valid_unsaved_thing.save
+        subject.save
+        subject.reload # grab new updated_at
+        subject.name = a_valid_unsaved_thing.name
+        subject.description = a_valid_unsaved_thing.description
+      end
+
+      it "should not raise an exception" do
+        expect { subject.save }.to_not raise_error
+      end
+
+      it "should return false" do
+        subject.save.should be_false
+      end
+
+      it "should not have stored the value in the database" do
+        expect do
+          subject.save
+          subject.reload
+        end.to_not change { subject.updated_at }
+      end
+
+      it "should have added an errors entry for the scoped attributes" do
+        subject.save
+        subject.errors.messages.should eq( a_widget_with_a_multi_column_uniqueness_violation.errors.messages )
+      end
+    end
+
+  end
+
 end
